fc26d71965f8e5dce2cf828f72cf6a92346341ea
bernard-geraud <bernard.dongmo@kaeyros-analytics.com>
2024-10-12 12:01:32 +0100

    Hibernate manual strategy for generated value

---
+++ src/main/java/com/devskills/springdata/models/Author.java
@@ -0,0 +1,65 @@
+package com.devskills.springdata.models;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity
+// @Table(name = "AUTHOR_TBL")
+public class Author {
+	
+	@Id
+	@GeneratedValue
+	/*(
+			strategy = TABLE,
+			generator = "author_id_gen"
+	)*/
+	/*@SequenceGenerator(
+			name = "author_sequence",
+			sequenceName = "author_sequence",
+			allocationSize = 1
+	)*/
+	/*@TableGenerator(
+			name = "author_id_gen",
+			table = "author_id_gen",
+			pkColumnName = "id_name",
+			valueColumnName = "id_value",
+			allocationSize = 1
+	)*/
+	private Integer id;
+	
+	@Column(
+			name = "f_name",
+			length = 35
+	)
+	private String firstName;
+	
+	private String lastName;
+	
+	@Column(
+			unique = true,
+			nullable = false
+	)
+	private String email;
+	
+	private int age;
+	
+	@Column(
+			updatable = false,
+			nullable = false
+	)
+	private LocalDateTime createdAt;
+	
+	@Column(
+			insertable = false
+	)
+	private LocalDateTime lastModified;
+
+}
