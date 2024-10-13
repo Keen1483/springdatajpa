74349a424e624cc544e88a67a6f47084a0a22da4
bernard-geraud <bernard.dongmo@kaeyros-analytics.com>
2024-10-13 08:29:15 +0100

    @Inheritance(strategy=SINGLE_TABLE) is usefull when we have a bit of subclass with a bit of difference within with parent class, strategy=JOINED is usefull when we have a bit of subclass with a lot of difference within with parent class, strategy=TABLE_PER_CLASS is usefull when we have a lot of subclass with a lot of difference within with the superclass.

--- src/main/java/com/devskills/springdata/SpringdataApplication.java
+++ src/main/java/com/devskills/springdata/SpringdataApplication.java
@@ -1,7 +1,13 @@
 package com.devskills.springdata;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.devskills.springdata.models.Video;
+import com.devskills.springdata.repositories.AuthorRepository;
+import com.devskills.springdata.repositories.VideoRepository;
 
 @SpringBootApplication
 public class SpringdataApplication {
@@ -10,20 +16,27 @@ public class SpringdataApplication {
 		SpringApplication.run(SpringdataApplication.class, args);
 	}
 	
-	/*@Bean
+	@Bean
 	CommandLineRunner commandLineRunner(
-			AuthorRepository repository
+			AuthorRepository authorRepository,
+			VideoRepository videoRepository
 	) {
 		return args -> {
-			var author = Author.builder()
+			/*var author = Author.builder()
 			.firstName("Jim")
 			.lastName("Karter")
 			.age(34)
 			.email("jim@mail.com")
 			.build();
 			
-			repository.save(author);
+			authorRepository.save(author);*/
+			
+			var video = Video.builder()
+					.name("abc")
+					.length(5)
+					.build();
+			videoRepository.save(video);
 		};
-	}*/
+	}
 
 }
--- src/main/java/com/devskills/springdata/models/Resource.java
+++ src/main/java/com/devskills/springdata/models/Resource.java
@@ -1,21 +1,29 @@
 package com.devskills.springdata.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
-@EqualsAndHashCode(callSuper = true)
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @SuperBuilder
 @Entity
-public class Resource extends BaseEntity {
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+// @DiscriminatorColumn(name = "resource_type") -> only with SINGLE_TABLE
+public class Resource {
+	
+	@Id
+	@GeneratedValue
+	private Integer id;
 	
 	private String name;
 	
